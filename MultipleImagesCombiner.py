0b31b3cd6ccb6507806d3101aea4beea09c71cfa
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2024-05-07 09:49:22 +0545

    add: Multiple images to one image combiner

--- MultipleImagesCombiner.py
+++ MultipleImagesCombiner.py
@@ -5,13 +5,13 @@ import numpy as np
 import os
 from math import floor
 
-folder = "/res/"
-out_file_name = "temp.png"
+folder = "/res/" # folder containing the images to combine
+out_file_name = "temp.png" 
 stack_axis = 1 # 0 for vertical, 1 for horizontal
-log_enable = True
+log_enable = True 
 should_resize = False #  all image of same dimension ?
 BG_CLIP_COLOR = [255, 255, 255] # if resize diable which color to put in bg 
-# BG_CLIP_COLOR = [0,0,0] # if resize diable which color to put in bg 
+# BG_CLIP_COLOR = [0,0,0] 
 
 file_name_list = list()
 image_list = list()
@@ -47,11 +47,10 @@ for ind, img in enumerate(image_list):
         else:
             if image_width_list[ind] - min_width < 0:
                 border_width = int(floor(abs(min_width-image_width_list[ind])/2))
-                print(border_width)
+
                 if (int(abs(min_width-image_width_list[ind]))/2).is_integer():
                     image_list[ind] = cv2.copyMakeBorder(image_list[ind], 0, 0, border_width, border_width, cv2.BORDER_CONSTANT, value=BG_CLIP_COLOR) 
                 else:
-                    print("Is float scaling " + str(ind))
                     image_list[ind] = cv2.copyMakeBorder(image_list[ind], 0, 0, border_width+1, border_width, cv2.BORDER_CONSTANT, value=BG_CLIP_COLOR) 
 
 
@@ -61,7 +60,7 @@ for ind, img in enumerate(image_list):
         else:
             if abs(image_height_list[ind] - min_height) > 0:
                 border_height = int(floor(abs(min_height-image_height_list[ind])/2))
-                print(border_height)
+
                 if (int(abs(min_height-image_height_list[ind]))/2).is_integer():
                     image_list[ind] = cv2.copyMakeBorder(image_list[ind], border_height, border_height, 0, 0, cv2.BORDER_CONSTANT, value=BG_CLIP_COLOR)
                 else:
